908dcbaa6cfa76a6b7eb9e52cd3d1b76ea48e3b3
marcoantonioafernandes <marco.fernandes@gmail.com>
2024-08-13 10:53:47 -0300

    all-project

---
+++ app/pipeline/extract.py
@@ -0,0 +1,25 @@
+"""Módulo com as funções de extração de dados."""
+
+import glob  # biblioteca para listar arquivos
+import os  # biblioteca para manipular arquivos e pastas
+from typing import List
+
+import pandas as pd
+
+
+def extract_from_excel(path: str) -> List[pd.DataFrame]:
+    """
+    função para ler os arquivos de uma pasta data/input e retornar uma lista de dataframes.
+
+    args: input_path (str): caminho da pasta com os arquivos
+
+    return: lista de dataframes
+    """
+    all_files = glob.glob(os.path.join(path, "*.xlsx"))
+
+    data_frame_list = []
+    for file in all_files:
+        data = pd.read_excel(file)
+        data_frame_list.append(data)
+
+    return data_frame_list
---
+++ app/pipeline/transform.py
@@ -0,0 +1,10 @@
+"""Módulo com as funções de transformação."""
+
+from typing import List
+
+import pandas as pd
+
+
+def contact_data_frames(data_frame_list: List[pd.DataFrame]) -> pd.DataFrame:
+    """função para transformar uma lista de dataframes em um único dataframe."""
+    return pd.concat(data_frame_list, ignore_index=True)
